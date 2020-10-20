e1f500fefafe5b5e264acf9d83c60b1662ce152a
Elinor Kero <elinor.kero@cmeducations.se>
2020-10-20 16:51:44 +0200

    enqueued woocommerce child and changes color of cart text to blue

--- wp-content/themes/kota-modified/functions.php
+++ wp-content/themes/kota-modified/functions.php
@@ -1,5 +1,5 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
 function my_theme_enqueue_styles() {
     $parenthandle = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
     $theme = wp_get_theme();
@@ -10,6 +10,19 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_uri(),
         array( $parenthandle ),
         $theme->get('Version') // this only works if you have Version in the style header
+    ); 
+
+
+    $parenthandleWC = 'woo-parent-style'; 
+    $theme = wp_get_theme();
+    wp_enqueue_style( $parenthandleWC, get_template_directory_uri() . '/woocommerce.css', 
+        array(),
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style( 'woo-child-style', get_stylesheet_directory_uri() . '/woocommerce_child.css',
+        array( $parenthandleWC ),
+        $theme->get('Version')
     );
 }
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 ?>
